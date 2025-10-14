dc6fe62740e30e7c7da08ae87ab373a25acab8e1
Andrew <117618414+Andeez123@users.noreply.github.com>
2025-10-14 19:22:28 +0800

    modify graph

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,6 +1,7 @@
 var vg_1 = "total_launches.vg.json"
 var vg_2 = "cumulative_people.vg.json"
 var vg_3 = "cost-space-launch.vg.json"
+var vg_4 = "nasa-budget.vg.json"
 
 vegaEmbed("#map", vg_1).then(function (result){
     
@@ -11,5 +12,9 @@ vegaEmbed("#bar_chart", vg_2).then(function (result){
 }).catch(console.error);
 
 vegaEmbed("#bubble_chart", vg_3).then(function (result){
+    
+}).catch (console.error);
+
+vegaEmbed("#nasa_budget", vg_4).then(function (result){
     
 }).catch (console.error);

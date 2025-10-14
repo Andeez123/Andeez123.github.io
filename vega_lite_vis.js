42972a229b41a8b7291aa15202e468ec4ab84724
Andrew <117618414+Andeez123@users.noreply.github.com>
2025-10-14 17:28:53 +0800

    tweak visualizations

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,15 @@
 var vg_1 = "total_launches.vg.json"
 var vg_2 = "cumulative_people.vg.json"
+var vg_3 = "cost-space-launch.vg.json"
 
 vegaEmbed("#map", vg_1).then(function (result){
     
 }).catch(console.error);
 
 vegaEmbed("#bar_chart", vg_2).then(function (result){
+    
+}).catch(console.error);
+
+vegaEmbed("#bubble_chart", vg_3).then(function (result){
     
 }).catch (console.error);
